691edf96b59d8b35cd75b20a0db9138220d1a058
A.S. Aziz <asazizmn@gmail.com>
2021-03-03 11:58:54 +0800

    change ListUsers functional component to a class one

--- src/ListUsers.js
+++ src/ListUsers.js
@@ -2,29 +2,43 @@ import React, { Component } from 'react';
 
 
 
-const ListUsers = props => (
-    <React.Fragment>
+class ListUsers extends Component {
+    render() {
+        return (
+            <React.Fragment>
 
-        <h3>List of users</h3>
+                <h3>List of users</h3>
 
-        <ol>
-            {
-                // display list of users
-                props.users.map((item, index) => (
-                    <li key={index}>{item.username} played {item.numberGames} games</li>
-                ))
-            }
-        </ol>
+                <ol>
+                    {
+                        // display list of users
+                        this.props.users.map((item, index) => (
+                            <li key={index}>{item.username} played {item.numberGames} games</li>
+                        ))
+                    }
+                </ol>
 
-        <div>
-            {
-                // display error in case of empty list
-                props.users.length === 0 && "There are currently no users listed"
-            }
-        </div>
+                <div>
+                    {
+                        // display error in case of empty list
+                        this.props.users.length === 0 && "There are currently no users listed"
+                    }
+                </div>
 
-    </React.Fragment>
-);
+                {
+                    // display error in case of empty list
+                    this.props.users.length !== 0 &&
+                    <button>
+                        Hide the Number of Games Played
+            </button>
+                }
+
+
+            </React.Fragment>
+            // );
+        )
+    }
+}
 
 
 
